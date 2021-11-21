958182357390b4f85270ab15288655ec014299ab
Movedizo <ramareid@hotmail.com>
2021-11-21 10:20:06 -0300

    Emprolijando

--- Controller/ApiComentController.php
+++ Controller/ApiComentController.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once "./Model/ComentsModel.php";
-require_once "./Model/ReacondicionadoModel.php";
 require_once "./View/ApiView.php";
 require_once "./Helpers/AccesoHelper.php";
 
@@ -12,20 +11,25 @@ class ApiComentController{
     private $accesoHelper;
 
     function __construct(){
+
         $this->accesoHelper = new AccesoHelper();
         $this->model = new ComentsModel();
         $this->view = new ApiView();
+
     }
 
     function getComents($params = null){
+
         $idComent = $params[':ID'];
+
         if(!isset($idComent)){
             $coments = $this->model->GetComents();
-           if($coments){
+            if($coments){
                 return $this->view->response($coments, 200);
             } 
             else return $this->view->response("Sin Comentarios", 400);
         }
+
         else {
             $coment = $this->model->GetComent($idComent);
             if($coment){
@@ -36,14 +40,35 @@ class ApiComentController{
     }
 
     function createComent(){
+
         $logueado = $this->accesoHelper->checkLoggedIn();
         if($logueado >= 1){
-         $body = $this->getBody();
-         $this->model->createComent($body->comentario, $body->puntaje, $body->fecha, $body->id_reacondicionado, $body->id_usuario);
+            $body = $this->getBody();
+            $id = $this->model->createComent($body->comentario, $body->puntaje, $body->fecha, $body->id_reacondicionado, $body->id_usuario);
+            if ($id != 0) {
+                $this->view->response("El comentario se insertó con el id=$id", 200);
+            } 
+            else {
+                $this->view->response("El comentario no se pudo insertar", 500);
+            }
+        }
+    }
+
+    function deleteComent($params = null){
+
+        $idComent = $params[":ID"];
+        $coment = $this->model->getComents($idComent);
+
+        if ($coment) {
+            $this->model->getComents($idComent);
+            return $this->view->response("El comentario con el id=$idComent fue eliminado", 200);
+        } else {
+            return $this->view->response("El comentario con el id=$idComent no existe", 404);
         }
     }
 
     private function getBody() {
+
         $bodyString = file_get_contents("php://input");
         return json_decode($bodyString);
     }
--- Model/ComentsModel.php
+++ Model/ComentsModel.php
@@ -8,24 +8,30 @@ class ComentsModel{
         $this->db = new PDO('mysql:host=localhost;' . 'dbname=csv_db 6;charset=utf8', 'root', '');
     }
 
-    function getComent($idComent){
-        $sentencia = $this->db->prepare( "SELECT * FROM comentarios WHERE id_reacondicionado= ?" );
-        $sentencia->execute(array($idComent));
+    function getComent($id){
+        $sentencia = $this->db->prepare("SELECT * FROM comentarios WHERE id = ?");
+        $sentencia->execute(array($id));
         $coment = $sentencia->fetch(PDO::FETCH_OBJ);
         return $coment;
     }
 
-    function getComents(){
-        $sentencia = $this->db->prepare( "SELECT * FROM comentarios");
-        $sentencia->execute();
+    function getComents($id_reacondicionado){
+        $sentencia = $this->db->prepare("SELECT comentarios.*, usuario.usuario FROM comentarios LEFT JOIN usuario
+                ON comentarios.id_usuario = usuario.id_usuario
+                WHERE id_reacondicionados = ?" );
+        $sentencia->execute($id_reacondicionado);
         $coments = $sentencia->fetchAll(PDO::FETCH_OBJ);
         return $coments;
     }
 
     function createComent($comentario, $puntaje, $fecha, $id_reacondicionado, $id_usuario){
-        $sentencia = $this->db->prepare("INSERT INTO comentarios(id_usuario, id_reacondicionado, comentario, puntaje, fecha) VALUES(?,?,?,?,?");
+        $sentencia = $this->db->prepare("INSERT INTO comentarios(id, id_usuario, id_reacondicionado, comentario, puntaje, fecha) VALUES (NULL, ?, ?, ?, ?, ?");
         $sentencia->execute(array($comentario, $puntaje, $fecha, $id_reacondicionado, $id_usuario));
+        return $this->db->lastInsertId();
+    }
+
+    function deleteComent($id){
+        $sentencia = $this->db->prepare("DELETE FROM comentarios WHERE id = ?");
+        $sentencia->execute(array($id));
     }
 }
-
-?>
